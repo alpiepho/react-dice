7f4186c0f9335feeedd95e692fa0bd2b26c8cf71
apiepho <alpiepho@gmail.com>
2019-06-21 16:54:17 -0600

    add link to github source

--- src/App.js
+++ src/App.js
@@ -16,6 +16,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>React-Dice
+        &nbsp;&nbsp;
+        <a
+          rel="noopener noreferrer"
+          href="https://github.com/alpiepho/react-dice"
+          target="_blank"
+        >
+        <i className="fa fa-github" aria-hidden="true" />
+        </a>        
+        </h3>
+
         <div style={{ display: 'flex', margin: 20 }}>
           <img src={firstDieImage} className="die" alt="Die one" />
           <img src={secondDieImage} className="die" alt="Die two" />

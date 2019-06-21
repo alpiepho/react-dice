968f9ef6740cd8c5e0b0ee6c4da19e323093871b
apiepho <alpiepho@gmail.com>
2019-06-21 15:09:11 -0600

    Updates

---
+++ precache-manifest.d7c4bf52b8cff915c84d3135ef44ff65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39ffac634b0f749003f17800cd9c43f",
+    "url": "/react-dice/index.html"
+  },
+  {
+    "revision": "385d9660dd15a53c17d0",
+    "url": "/react-dice/static/css/main.8d65c69f.chunk.css"
+  },
+  {
+    "revision": "8568f50f7ceb80227548",
+    "url": "/react-dice/static/js/2.d42b6f0b.chunk.js"
+  },
+  {
+    "revision": "385d9660dd15a53c17d0",
+    "url": "/react-dice/static/js/main.1bbf859b.chunk.js"
+  },
+  {
+    "revision": "b53b16400a8a29e4d3fd",
+    "url": "/react-dice/static/js/runtime~main.9b5673c1.js"
+  }
+]);

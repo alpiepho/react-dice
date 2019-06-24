5f0a62190a90237c1551bfdc6d0fa14ed9367a2c
apiepho <alpiepho@gmail.com>
2019-06-24 07:50:45 -0600

    Updates

--- reactdice-sw.js
+++ reactdice-sw.js
@@ -21,6 +21,8 @@ self.addEventListener('install', (evt) => {
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
+    }).catch(function(err) {
+      console.log(err);
     })
   );
   self.skipWaiting();

5c7068ec39dccd8b1dcad466e462c63e5367c849
apiepho <alpiepho@gmail.com>
2019-06-25 11:36:47 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-dice/precache-manifest.2e9479ec1dd512f53357daa444d560d2.js"
+  "/react-dice/precache-manifest.d1d894357f4d31effe542862cb6f9598.js"
 );
 
 self.addEventListener('message', (event) => {
